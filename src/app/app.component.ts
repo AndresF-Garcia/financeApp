ce489b79394235c6d337f1d1e8a8c9b59a75d7f0
Andres <naranjoaf01@gmail.com>
2024-09-07 16:11:17 -0500

    Creación e implementacion del menu para mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MobileMenuComponent } from '@shared/mobile-menu/mobile-menu.component';
 import { TopBarComponent } from '@shared/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent],
+  imports: [RouterOutlet, TopBarComponent, MobileMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/mobile-menu/mobile-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MenuItem } from './interfaces/menu-item.interface';
+import { PrimeIcons } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'shared-mobile-menu',
+  standalone: true,
+  imports: [RouterModule],
+  styleUrl: './mobile-menu.component.css',
+  templateUrl: './mobile-menu.component.html',
+})
+export class MobileMenuComponent {
+
+  menuItems: MenuItem[] = [
+    {
+      name: 'Home',
+      icon: PrimeIcons.HOME,
+      route: 'transactions'
+    }
+  ]
+
+}
