583ee437b6bdf613110637587b2a40acfa53aaac
Andres <naranjoaf01@gmail.com>
2024-09-08 21:54:48 -0500

    Comentarios de routerControlService

--- src/app/services/route-control.service.ts
+++ src/app/services/route-control.service.ts
@@ -2,6 +2,10 @@ import { computed, Injectable, signal } from '@angular/core';
 import { Route } from '@angular/router';
 import { routes } from '../app.routes';
 
+/*
+Servicio para control de el estado de las rutas
+*/
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,13 +17,24 @@ export class RouteControlService {
   public currentRoute = computed<Route|null>(() => this._currentRoute());
   public previousRoute = computed<Route|null>(() => this._previousRoute());
 
-  constructor() { }
+  constructor() {
+    this.changeRouteHistory('Transacciones');
+  }
 
+  /**
+   * Actualiza el estado de las rutas
+   * @param newRouteTitle titulo de la nueva ruta a la que se navega
+   */
   public changeRouteHistory(newRouteTitle:string):void {
     this._previousRoute.set(this._currentRoute());
     this._currentRoute.set(this.getRouteItemByTitle(newRouteTitle));
   }
 
+  /**
+   * Busca una ruta por su titulo
+   * @param title titulo de la ruta a buscar en el Objeto routes (app.routes.ts)
+   * @returns item de el objeto routes
+   */
   private getRouteItemByTitle(title:string): Route {
 
     const allFoundRoutes = routes
@@ -31,6 +46,5 @@ export class RouteControlService {
     return allFoundRoutes[0];
 
   }
-
 
 }
